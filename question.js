5802a802039dd24413ce7604205da9db1b9ae5a4
PreetiDiwakarGitHub <p208016@gmail.com>
2024-05-17 13:56:46 +0530

    first

---
+++ question.js
@@ -0,0 +1,51 @@
+let inputElement = document.getElementById('input');
+let button = document.getElementById('btn');
+let output = document.getElementById('output');
+let revBtn = document.getElementById('reverse');
+let WordBtn = document.getElementById('reWord');
+
+button.addEventListener('click', () => {
+    let inputVal = inputElement.value;
+    let res = capitalize(inputVal);
+    output.innerHTML = `<h2>${res}</h2>`;
+});
+
+revBtn.addEventListener('click', () => {
+    let inputVal = inputElement.value;
+    let res = reverse(inputVal);
+    output.innerHTML = `<h2>${res}</h2>`;
+});
+
+WordBtn.addEventListener('click', () => {
+    let inputVal = inputElement.value;
+    let res = reverseWords(inputVal);
+    output.innerHTML = `<h2>${res}</h2>`;
+});
+
+function capitalize(str) {
+    let s = '';
+    for (let i = 0; i < str.length; i++) {
+        if (i === 0 || str[i - 1] === ' ') {
+            s += str[i].toUpperCase();
+        } else {
+            s += str[i];
+        }
+    }
+    return s;
+}
+
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+function reverseWords(str) {
+    return str.split(' ').map(word => word.split('').reverse().join('')).join(' ');
+}
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/PreetiDiwakarGitHub/bom-object.git
+// git push -u origin main
